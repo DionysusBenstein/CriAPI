142633eb7de933abede10fd743ec85ca2098f43c
DionysusBenstein <dionysusbenstein@gmail.com>
2021-08-07 11:56:46 +0300

    Use CORS policy

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+var cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors())
 
 const wallets = [
     "bc1q6w0eufkfhya4xjpyqnvkwcdt9a2kca8am097um", // wallet_1
